845e7ddc23301b67344d6627f063c125785e0f72
Goofy.L <goofy.l@GoofyL-H-23PC-MP16-331.local>
2025-10-21 22:07:55 +0900

    1st commit

---
+++ src/test/java/org/example/jpastudy/JpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
